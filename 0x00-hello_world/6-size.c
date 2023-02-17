ebdb73ff30b7b1a3debd3fafae1c80b362848f14
Sphesihle <dsihledlamini10@gmail.com>
2023-02-17 00:47:20 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(h));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printif("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
