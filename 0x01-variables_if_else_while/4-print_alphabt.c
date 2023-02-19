ba59fb860c1cf67fa8ba0e4c32b46087c8157187
Sphesihle <dsihledlamini10@gmail.com>
2023-02-19 14:57:38 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	/* your code goes there */
+	char j;
+
+	for (j = 'a'; j != 'z'; j++)
+		if (j != 'p' && j != 'e')
+		putchar(j);
+		putchar('|');
+		return (0);
+}
