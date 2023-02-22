e4f93d1063f9f03cb463403a895cffef31c937d2
Sphesihle <dsihledlamini10@gmail.com>
2023-02-21 20:24:23 -0800

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -6,8 +6,16 @@
  * Return: Always 0
  */
 int main(void)
+
 {
-	putchar('i');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
