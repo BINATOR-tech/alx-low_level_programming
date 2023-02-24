769782bab3d0ca6e72d23cc59975264267dd5bee
Sphesihle <dsihledlamini10@gmail.com>
2023-02-24 04:47:36 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - Test for positive or negative
  * Return: Always 0
  */
+
 int main(void)
+
 {
 	int i;
-	i = 0
+
+	i = 0;
 
 	positive_or_negative(i);
 	return (0);
