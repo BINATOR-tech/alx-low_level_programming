f86b296113723d391b94e5df480ec57807f5c7ec
Sphesihle <dsihledlamini10@gmail.com>
2023-03-08 13:35:58 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,6 +9,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	puts("\n");
+	if (*s == '\0')
+	putchar('\n');
+	else
+	{
+	putchar(s[0]);
+	_puts_recursion(s + 1);
+	}
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of n
+ * @int
+ * 
+ * Return: Success 0 On error, -1 is returned
+ */
